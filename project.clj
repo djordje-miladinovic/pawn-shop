940fea06af63f899502c39c06ca88520fa420746
Djordje <miladinovic.djordje@gmail.com>
2021-03-10 22:56:00 +0100

    Added Lein-Ring plugin for web server starting; Displaying simple Hello page

--- project.clj
+++ project.clj
@@ -4,6 +4,9 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.1"]
-                 [ring/ring-core "1.9.1"]
+                 [ring "1.9.1"]
                  [compojure "1.6.2"]]
-  :repl-options {:init-ns pawn-shop.core})
+  :repl-options {:init-ns pawn-shop.core}
+  :plugins [[lein-ring "0.12.5"]]
+  :ring {:handler pawn-shop.core/handler}
+)
